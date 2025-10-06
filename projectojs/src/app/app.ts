c7692eedcbe5974383d28dc6190d085c1c4ff2ea
ABMBE <ermacsebas@gmail.com>
2025-10-05 23:30:01 -0500

    aprendiendo

--- projectojs/src/app/app.ts
+++ projectojs/src/app/app.ts
@@ -9,4 +9,23 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('projectojs');
+
+  nombre = "Sebastian"
+  description = 'Proyecto con JS y Angular'
+  resultado: string = ''
+
+  cambiarNombre() {
+    this.nombre = "Juan"
+  }
+
+  contar() {
+    this.resultado = ''
+    for (let i = 1; i<=10; i++){
+      this.resultado += i + ' '
+    }  
+  }
+
+  
+  
+
 }
